75bb6fec052f9f93f74015389faac2f98ed76edd
William Chandler <williamchandler@Williams-MacBook-Pro.local>
2025-05-12 13:50:17 -0500

    updated: git file

--- url-shortner-frontend/src/api/api.js
+++ url-shortner-frontend/src/api/api.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+// Create an axios instance with a base URL
+// This base URL points to the backend server where the API is hosted
+// The base URL is set to the Render.com deployment of the URL shortener service
 export default axios.create({
-    baseURL: `http://localhost:8080`
+    baseURL: `https://url-shortner-sb-1.onrender.com`,
 })
